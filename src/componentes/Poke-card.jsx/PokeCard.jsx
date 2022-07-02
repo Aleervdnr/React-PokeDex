dcf05804146af765343e8eac6428e7a0ea719586
Alejo <alejonahuelrott@gmail.com>
2022-07-01 23:12:14 -0300

    add cardModal

--- src/componentes/Poke-card.jsx/PokeCard.jsx
+++ src/componentes/Poke-card.jsx/PokeCard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./PokeCard.css"
 
 function PokeCard({id,name,avatar,types}) {
+  const [modal, setModal] = useState(false)  
+
   return (
-    <div className={`pokeCard ${types[0].type.name}` }>
+    <>
+          <div className={`pokeCard ${types[0].type.name}`} onClick={()=> setModal(true)}>
       <img src={avatar} alt={name} className="pokeCard_img"/>
 
       <div className="poke-container">
@@ -15,11 +18,19 @@ function PokeCard({id,name,avatar,types}) {
 
         <span className='pokeCard_id'>#{id}</span>
       </div>
+    </div>
 
-      <div className="pokeCard_modal">
+    <div className={modal ? "pokeCard_modal showModal" : "pokeCard_modal"} >
+        <div className='cardModal'>
+          <div className="close" onClick={()=> setModal(false)}>X</div>
+          <h1>{`${name}`}</h1>
+          <img src={avatar} alt=""/>
+        </div>
+        
         
       </div>
-    </div>
+    </>
+
   )
 }
 
