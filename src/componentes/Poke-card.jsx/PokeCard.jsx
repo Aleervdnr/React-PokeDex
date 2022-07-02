970c02bb8b113d9b8c544822dd7e4badda7e80bb
Alejo <alejonahuelrott@gmail.com>
2022-07-02 00:04:07 -0300

    add no scroll

--- src/componentes/Poke-card.jsx/PokeCard.jsx
+++ src/componentes/Poke-card.jsx/PokeCard.jsx
@@ -6,7 +6,7 @@ function PokeCard({id,name,avatar,types}) {
 
   return (
     <>
-          <div className={`pokeCard ${types[0].type.name}`} onClick={()=> setModal(true)}>
+          <div className={`pokeCard ${types[0].type.name}`} onClick={()=> {setModal(true); document.body.classList.add("no-scroll")}}>
       <img src={avatar} alt={name} className="pokeCard_img"/>
 
       <div className="poke-container">
@@ -22,7 +22,7 @@ function PokeCard({id,name,avatar,types}) {
 
     <div className={modal ? "pokeCard_modal showModal" : "pokeCard_modal"} >
         <div className='cardModal'>
-          <div className="close" onClick={()=> setModal(false)}>X</div>
+          <div className="close" onClick={()=>{ setModal(false); document.body.classList.remove("no-scroll")}}>X</div>
           <h1>{`${name}`}</h1>
           <img src={avatar} alt=""/>
         </div>
